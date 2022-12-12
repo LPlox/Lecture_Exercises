44b0cab118d7fefa18846a85097a3305778f3317
LPlox <lina.js.wang@gmail.com>
2022-12-12 15:31:44 +0100

    fix: typos 6, 7

--- exercises/day1/e6_or.js
+++ exercises/day1/e6_or.js
@@ -7,15 +7,23 @@
  * Combine the two if statements into one statement which returns the string
  * Outside if val is not between 10 and 20, inclusive. Otherwise, return the string Inside.
  */
-
 function testLogicalOr(val) {
 	// Only change code below this line
+
+	if (val) {
+		return "Outside";
+	}
+
+	if (val) {
+		return "Outside";
+	}
 
 	// Only change code above this line
 	return "Inside";
 }
 
-console.log(testLogicalOr(5)); //should return the string Outside
-console.log(testLogicalOr(6)); //should return the string Outside
-console.log(testLogicalOr(24)); //should return the string Outside
-console.log(testLogicalOr(10)); //should return the string Inside
+console.log(testLogicalOr(0)); //should return the string Outside
+console.log(testLogicalOr(10)); // should return the string Inside
+console.log(testLogicalOr(15)); // should return the string Inside
+console.log(testLogicalOr(19)); // should return the string Inside
+console.log(testLogicalOr(20)); // should return the string Inside
--- exercises/day1/e7_and.js
+++ exercises/day1/e7_and.js
@@ -1,27 +1,25 @@
 /**
  * Logical operators (AND)
  * 69 COMPARISONS WITH THE LOGICAL AND OPERATOR
- * Sometimes you will need to test more than one thing at a time. 
- * The logical and operator (&&) returns true if and only if the 
+ * Sometimes you will need to test more than one thing at a time.
+ * The logical and operator (&&) returns true if and only if the
  * operands to the left and right of it are true.
- * 
+ *
  * TASK
- * Replace the two if statements with one statement, using the && operator, 
- * which will return the string Yes if val is less than or equal to 50 and 
+ * Replace the two if statements with one statement, using the && operator,
+ * which will return the string Yes if val is less than or equal to 50 and
  * greater than or equal to 25. Otherwise, will return the string No.
  */
 
-
 function testLogicalAnd(val) {
-  // Only change code below this line
+	// Only change code below this line
 
-  // Only change code above this line
-  return "No";
+	// Only change code above this line
+	return "No";
 }
 
-
-console.log(testLogicalAnd(25)) //should return the string Yes
-console.log(testLogicalAnd(24)) //should return the string Yes
-console.log(testLogicalAnd(51)) //should return the string No
-console.log(testLogicalAnd(50)) //should return the string Yes
-console.log(testLogicalAnd(80)) //should return the string No
+console.log(testLogicalAnd(25)); //should return the string Yes
+console.log(testLogicalAnd(24)); //should return the string No
+console.log(testLogicalAnd(51)); //should return the string No
+console.log(testLogicalAnd(50)); //should return the string Yes
+console.log(testLogicalAnd(80)); //should return the string No
